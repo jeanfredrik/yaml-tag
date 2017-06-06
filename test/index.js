4e469707e4454a202c9a8719a5d00583c41b1603
jeanfredrik <jeanfredrik@gmail.com>
2017-06-06 18:17:46 +0200

    ✨ Add support for replacement expressions

--- test/index.js
+++ test/index.js
@@ -29,4 +29,26 @@ describe('parse', () => {
     `;
     expect(result).to.deep.equal(expectedResult);
   });
+  it('handles template expression with array', () => {
+    const expectedResult = {foo: [1, 2]};
+    const result = parse`
+      foo: ${[1, 2]}
+    `;
+    expect(result).to.deep.equal(expectedResult);
+  });
+  it('handles template expression with string', () => {
+    const expectedResult = {foo: 'bar'};
+    const result = parse`
+      foo: ${'bar'}
+    `;
+    expect(result).to.deep.equal(expectedResult);
+  });
+  it('handles template expression with function', () => {
+    const replacement = function() {};
+    const expectedResult = {foo: replacement};
+    const result = parse`
+      foo: ${replacement}
+    `;
+    expect(result).to.deep.equal(expectedResult);
+  });
 });
